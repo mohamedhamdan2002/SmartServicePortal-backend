9a1552d03193bc5c80711e2cf24a445d30d5f47f
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-18 22:58:16 +0200

    create 'Repositories' folder for baseRepository and RepositoryManager

---
+++ src/Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Specifications;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Domain.Abstractions;
+using Domain.Interfaces;
+namespace Infrastructure.Repositories;
+public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : Entity, BaseEntity
+{
+    private readonly AppDbContext _dbContext;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = dbContext.Set<TEntity>();
+    }
+    public async Task<int> CountAsync(Specification<TEntity> specification)
+        => await ApplaySpecifictaions(specification).CountAsync();
+    public void Create(TEntity entity)
+        => _dbContext.Add(entity);
+    public void Delete(TEntity entity)
+        => _dbContext.Remove(entity);
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+        => await _dbSet.ToListAsync();
+    public async Task<IEnumerable<TResult>> GetAllAsync<TResult>(Specification<TEntity, TResult> specification)
+        => await ApplaySpecifictaions(specification).ToListAsync();
+    public async Task<TEntity?> GetByIdAsync(int id)
+        => await _dbSet.FindAsync(id);
+    public async Task<TResult?> GetBySpecAsync<TResult>(Specification<TEntity, TResult> specification)
+        => await ApplaySpecifictaions(specification).FirstOrDefaultAsync();
+    public void Update(TEntity entity)
+        => _dbContext.Update(entity);
+    public async Task DeleteRangeAsync(IEnumerable<int> ids)
+        => await _dbSet.Where(e => ids.Any(id => e.Id == id)).ExecuteDeleteAsync();
+    private IQueryable<TResult> ApplaySpecifictaions<TResult>(Specification<TEntity, TResult> specification)
+        => _dbSet.GetQuery(specification);
+    private IQueryable<TEntity> ApplaySpecifictaions(Specification<TEntity> specification)
+        => _dbSet.GetQuery(specification);
+    public async Task<TEntity?> GetBySpecAsync(Specification<TEntity> specification)
+        => await ApplaySpecifictaions(specification).FirstOrDefaultAsync();
+
+}
---
+++ src/Infrastructure/Repositories/RepositoryManager.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Data;
+using System.Collections;
+using Domain.Abstractions;
+using Domain.Interfaces;
+
+namespace Infrastructure.Repositories;
+
+public class RepositoryManager : IRepositoryManager
+{
+    private readonly AppDbContext _appDbContext;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly Hashtable _repositories = new();
+    public RepositoryManager(
+            AppDbContext appDbContext,
+            IServiceProvider serviceProvider
+        )
+    {
+        _appDbContext = appDbContext;
+        _serviceProvider = serviceProvider;
+    }
+    public IRepository<Service> ServiceRepository => GetRepository<Service>();
+
+    public IRepository<Category> CategoryRepository => GetRepository<Category>();
+
+    public IRepository<Reservation> ReservationRepository => GetRepository<Reservation>();
+
+    public IRepository<Review> ReviewRepository => GetRepository<Review>();
+
+    private IRepository<TEntity> GetRepository<TEntity>() where TEntity : Entity
+    {
+        var type = typeof(TEntity).Name;
+        if (!_repositories.ContainsKey(type))
+        {
+            var repository = _serviceProvider.GetRequiredService<IRepository<TEntity>>();
+            _repositories.Add(type, repository);
+        }
+        return _repositories[type] as IRepository<TEntity>;
+    }
+    public async Task SaveChangesAsync()
+        => await _appDbContext.SaveChangesAsync();
+
+    public async ValueTask DisposeAsync()
+        => await _appDbContext.DisposeAsync();
+}
