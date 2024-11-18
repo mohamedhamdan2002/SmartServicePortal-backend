96ba010fc471f026fa85616156fd53aea8595833
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-18 22:51:15 +0200

    Rename Folder 'Repositories' to 'Interfaces'

---
+++ src/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Abstractions;
+using Domain.Specifications;
+
+namespace Domain.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : Entity
+{
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<int> CountAsync(Specification<TEntity> specification);
+    Task<IEnumerable<TResult>> GetAllAsync<TResult>(Specification<TEntity, TResult> specification);
+    Task<TResult?> GetBySpecAsync<TResult>(Specification<TEntity, TResult> specification);
+    Task<TEntity?> GetBySpecAsync(Specification<TEntity> specification);
+    Task<TEntity?> GetByIdAsync(int id);
+    void Create(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task DeleteRangeAsync(IEnumerable<int> ids);
+}
