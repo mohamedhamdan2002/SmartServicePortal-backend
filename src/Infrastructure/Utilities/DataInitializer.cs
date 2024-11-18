5a553c15b17828775ddfc96dd73a24e249966a23
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-18 22:59:41 +0200

    move DateInitialzer class in folder Utilities

---
+++ src/Infrastructure/Utilities/DataInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+using Infrastructure.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.Json;
+using System.Xml.XPath;
+
+namespace Infrastructure.Utilities;
+
+public static class DataInitializer
+{
+    private static readonly string _direcotry = Directory.GetCurrentDirectory();
+    static DataInitializer()
+    {
+        var baseDirectory = Directory.GetParent(Directory.GetCurrentDirectory());
+        if (baseDirectory is null)
+            throw new InvalidOperationException();
+        var currentProjectFolder = Path.Combine(baseDirectory.FullName, "Infrastructure");
+        var targetDirecotry = Path.Combine(currentProjectFolder, "DataSeed");
+        _direcotry = targetDirecotry;
+    }
+
+    public static async Task InitializeAsync(AppDbContext dbContext)
+    {
+
+        await SetData<Category>(dbContext, FilePath("categories.json"));
+        await SetData<Service>(dbContext, FilePath("services.json"));
+
+    }
+    private static IEnumerable<T>? GetDataFromFile<T>(string path) where T : Entity
+    {
+        if (!File.Exists(path))
+            throw new FileNotFoundException($"File Not Found At: ${path}");
+        var dataAsString = File.ReadAllText(path);
+        var Data = JsonSerializer.Deserialize<IEnumerable<T>>(dataAsString);
+        return Data;
+    }
+
+    private static async Task SetData<T>(AppDbContext dbContext, string path) where T : Entity
+    {
+        if (dbContext.Set<T>().Any()) return;
+        var data = GetDataFromFile<T>(path);
+        if (data is null || !data.Any())
+            throw new InvalidDataException("The data in the file is either null or empty.");
+        dbContext.AddRange(data);
+        await dbContext.SaveChangesAsync();
+    }
+    private static string FilePath(string fileName)
+        => Path.Combine(_direcotry, fileName);
+}
