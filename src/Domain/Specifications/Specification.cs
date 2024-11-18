33403f2ca2053370b1ee621303b863adc71c1820
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-18 22:53:59 +0200

    rename 'SpecificationWithResultType' to 'Specification'

--- src/Domain/Specifications/Specification.cs
+++ src/Domain/Specifications/Specification.cs
@@ -1,14 +1,11 @@
-﻿using Domain.Entities;
+﻿using Domain.Abstractions;
 using System.Linq.Expressions;
 
 namespace Domain.Specifications;
-
-public abstract class Specification<TEntity> where TEntity : IEntity
+public abstract class Specification<TEntity> where TEntity : BaseEntity
 {
     public Expression<Func<TEntity, bool>>? Predicate { get; set; }
-
     public ICollection<Expression<Func<TEntity, object>>> Includes { get; } = new List<Expression<Func<TEntity, object>>>();
-
     public Expression<Func<TEntity, object>>? OrderBy { get; set; }
     public Expression<Func<TEntity, object>>? OrderByDescending { get; set; }
     public bool AsNoTracking { get; set; } = true;
@@ -16,15 +13,9 @@ public abstract class Specification<TEntity> where TEntity : IEntity
     public int Skip { get; set; }
     public bool IsPaginationEnabled { get; set; }
 
-    public Specification()
-    {
-
-    }
+    public Specification() {}
     public Specification(Expression<Func<TEntity, bool>> predicate)
-    {
-        Predicate = predicate;
-    }
-
+        => Predicate = predicate;
     protected void AddInclude(Expression<Func<TEntity, object>> include)
         => Includes.Add(include);
     protected void AddPagination(int pageSize, int pageIndex)
@@ -33,4 +24,11 @@ public abstract class Specification<TEntity> where TEntity : IEntity
         Take = pageSize;
         IsPaginationEnabled = true;
     }
+}
+public abstract class Specification<TEntity, TResult> : Specification<TEntity> where TEntity : BaseEntity
+{
+    public Expression<Func<TEntity, TResult>>? Selector { get; set; }
+    protected Specification() {}
+    public Specification(Expression<Func<TEntity, bool>> predicate) 
+        : base(predicate) {}
 }
--- src/Domain/Specifications/SpecificationEvaluator.cs
+++ src/Domain/Specifications/SpecificationEvaluator.cs
@@ -1,18 +1,16 @@
-﻿using Domain.Entities;
+﻿using Domain.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Specifications;
 
 public static class SpecificationEvaluator
 {
-    private static bool hasSelector = false;
-    public static IQueryable<TResult> GetQuery<TEntity, TResult>(this IQueryable<TEntity> inputQuery, SpecificationWithResultType<TEntity, TResult> specification) where TEntity : class, IEntity
+    public static IQueryable<TResult> GetQuery<TEntity, TResult>(this IQueryable<TEntity> inputQuery, Specification<TEntity, TResult> specification) where TEntity : class, BaseEntity
     {
-        hasSelector = true;
         var query = inputQuery.GetQuery<TEntity>(specification);
-        return query.Select(specification.Selector);
+        return query.Select(specification.Selector!);
     }
-    public static IQueryable<TEntity> GetQuery<TEntity>(this IQueryable<TEntity> inputQuery, Specification<TEntity> specification) where TEntity : class, IEntity
+    public static IQueryable<TEntity> GetQuery<TEntity>(this IQueryable<TEntity> inputQuery, Specification<TEntity> specification) where TEntity : class, BaseEntity
     {
         var query = inputQuery;
 
@@ -20,34 +18,22 @@ public static class SpecificationEvaluator
             return query;
 
         if (specification.Predicate is not null)
-        {
             query = query.Where(specification.Predicate);
-        }
 
         query = specification?.Includes.Aggregate(query,
             (currentQuery, newIncludeExpression) => currentQuery.Include(newIncludeExpression));
 
-
         if (specification!.OrderBy is not null)
-        {
-            query = query.OrderBy(specification.OrderBy);
-        }
+            query = query!.OrderBy(specification.OrderBy);
 
         if (specification.OrderByDescending is not null)
-        {
-            query = query.OrderByDescending(specification.OrderByDescending);
-        }
+            query = query!.OrderByDescending(specification.OrderByDescending);
 
         if (specification!.IsPaginationEnabled)
-        {
             query = query!.Skip(specification.Skip).Take(specification.Take);
-        }
 
-
-        if (specification.AsNoTracking && !hasSelector)
-        {
+        if (specification.AsNoTracking)
             query = query!.AsNoTracking();
-        }
         return query!;
     }
 }
