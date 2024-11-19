785488e6e6c58c8547604ab00a46b0a9819d8170
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-19 20:58:24 +0200

    Add Filter To bind UserId from user Claims

--- src/Api/Filters/BindFromClaimsAttribute.cs
+++ src/Api/Filters/BindFromClaimsAttribute.cs
@@ -5,15 +5,15 @@ namespace Api.Filters;
 
 public class BindFromClaimsAttribute : Attribute, IAsyncActionFilter
 {
-    public string Claim { get; set; }
-    public string ParameterName { get; set; }
+    public required string Claim { get; set; }
+    public required string ParameterName { get; set; }
+
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
         var user = context.HttpContext.User;
         if (user.Identity.IsAuthenticated)
         {
             var claim = user.FindFirstValue(Claim);
-
             if (!string.IsNullOrEmpty(claim) &&
                 !string.IsNullOrEmpty(ParameterName))
             {
