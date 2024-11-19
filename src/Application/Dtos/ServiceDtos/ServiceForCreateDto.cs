141c81b878f1677a058d88accaf80e05f140a46b
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-19 16:59:01 +0200

    add mapping methods in serviceDtos

--- src/Application/Dtos/ServiceDtos/ServiceForCreateDto.cs
+++ src/Application/Dtos/ServiceDtos/ServiceForCreateDto.cs
@@ -1,3 +1,19 @@
-﻿namespace Application.Dtos.ServiceDtos;
+﻿using Domain.Entities;
 
-public record ServiceForCreateDto : ServiceForManipulationDto;
+namespace Application.Dtos.ServiceDtos;
+
+public record ServiceForCreateDto : ServiceForManipulationDto 
+{ 
+    public Service ToEntity(string? pictureUrl = null)
+    {
+        var service = new Service()
+        {
+            CategoryId = this.CategoryId,
+            Cost = this.Cost,
+            Description = this.Description,
+            Name = this.Name
+        };
+        if( pictureUrl != null ) service.PictureUrl = pictureUrl;
+        return service;
+    }
+}
--- src/Application/Dtos/ServiceDtos/ServiceForUpdateDto.cs
+++ src/Application/Dtos/ServiceDtos/ServiceForUpdateDto.cs
@@ -1,3 +1,15 @@
-﻿namespace Application.Dtos.ServiceDtos;
+﻿using Domain.Entities;
 
-public record ServiceForUpdateDto : ServiceForManipulationDto;
+namespace Application.Dtos.ServiceDtos;
+
+public record ServiceForUpdateDto : ServiceForManipulationDto
+{
+    public void UpdateEntity(Service service, string? pictureUrl = null)
+    {
+        service.Name = this.Name;
+        service.Cost = this.Cost;
+        service.Description = this.Description;
+        if (pictureUrl != null) 
+            service.PictureUrl = pictureUrl;
+    }
+}
