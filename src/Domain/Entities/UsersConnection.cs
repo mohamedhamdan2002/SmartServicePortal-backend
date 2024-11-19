83ac1e2bde6398c75408204413d48f4cab92c6c8
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-19 20:47:01 +0200

    add userConnections Table

---
+++ src/Domain/Entities/UsersConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstractions;
+
+namespace Domain.Entities
+{
+    public class UsersConnection : Entity
+    {
+        public string UserId { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
