1936d0edfeec35ee2fc367a584b49eb14f35d1c4
mohamedhemdan <mohamedsmsma3s@gmail.com>
2024-11-19 20:40:46 +0200

    refactor BasApiController

--- src/Api/Controllers/BaseApiController.cs
+++ src/Api/Controllers/BaseApiController.cs
@@ -1,7 +1,5 @@
 ﻿using Api.Utilities;
 using Domain.Errors;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -10,31 +8,47 @@ namespace Api.Controllers;
 [ApiController]
 public class BaseApiController : ControllerBase
 {
-    public ActionResult HandleError(Error error)
+    protected ActionResult HandleError(Error error)
     {
+        if (error is null || error.StatusCode == StatusCodes.Status200OK)
+            throw new ArgumentNullException(nameof(error));
+
         return error.StatusCode switch
         {
             StatusCodes.Status404NotFound => NotFound(error),
             StatusCodes.Status400BadRequest => BadRequest(error),
             StatusCodes.Status401Unauthorized => Unauthorized(error),
             _ => throw new NotImplementedException()
-        }; ;
+        };
     }
 
-    public ActionResult HandleResult<TResult>(Result result, ActionEnum action = ActionEnum.OkResult)
+    protected ActionResult HandleResult(Result result)
     {
-        if (result is null)
-            throw new ArgumentNullException(nameof(result));
+        ValdateResult(result);
+        if (result.IsFailure)
+            return HandleError(result.Error);
+        return NoContent();
+    }
+
+    protected ActionResult HandleResult<TResult>(Result<TResult> result, ActionEnum action = ActionEnum.OkResult)
+    {
+        ValdateResult(result);
 
         if (result.IsFailure)
             return HandleError(result.Error);
 
         return action switch
         {
-            ActionEnum.OkResult => Ok(result.GetData<TResult>()),
-            ActionEnum.NoContentResult => NoContent(),
-            ActionEnum.CreatedAtResult => Ok(result.GetData<TResult>()), // will be change
+            ActionEnum.OkResult => Ok(result.Data),
+            ActionEnum.CreatedAtResult => StatusCode(StatusCodes.Status201Created, result.Data),
             _ => throw new NotImplementedException()
         };
     }
+
+    private void ValdateResult(Result result)
+    {
+        if (result is null)
+            throw new ArgumentNullException(nameof(result));
+    }
+
 }
--- src/Api/Utilities/ApiConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Api.Utilities;
-
-public static class ApiConstants
-{
-    public const string DefaultConnection = nameof(DefaultConnection);
-    public const string MyAppPolicy = nameof(MyAppPolicy);
-}
